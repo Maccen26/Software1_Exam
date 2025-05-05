70de71c0427ad2d2e52bf5eecc1d08ede593f3a5
Lukas <s234928@dtu.dk>
2025-05-05 16:50:30 +0200

    Fixed some merge conflicts

--- src/main/java/dtu/ui/Ui.java
+++ src/main/java/dtu/ui/Ui.java
@@ -8,17 +8,7 @@ import dtu.app.*;
 import dtu.domain.*;
 
 public class Ui {
-<<<<<<< HEAD
-    private static App app = new App(new ArrayList<Project>(), new ArrayList<Developer>());
-
-    
-=======
-    protected App app = new App();
-    protected static LoginFrame loginFrame = new LoginFrame();
-    protected static CreateProjectFrame createProjectFrame = new CreateProjectFrame();
-    protected static EditActivityFrame editActivityFrame = new EditActivityFrame();
-    protected static GetReportFrame getReportFrame = new GetReportFrame();
->>>>>>> 0cfe1e5ad15c0123a2fa57f42336905199004cac
+    private static App app = new App();
     
     public static void main(String[] args) { // Lukas
         LoginFrame loginFrame = new LoginFrame(app);
