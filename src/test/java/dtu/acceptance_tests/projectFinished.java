3b0ecd88e889c2a55539b0fccf8faf4d3a1b88c6
frej02 <s234924@dtu.dk>
2025-05-05 16:41:55 +0200

    slettet fejl

--- src/test/java/dtu/acceptance_tests/projectFinished.java
+++ src/test/java/dtu/acceptance_tests/projectFinished.java
@@ -17,39 +17,37 @@ public class ProjectFinished {
 
     @Given("a project {string}")
     public void aProject(String string){
-        app.createProject();
-        project = app.getProject(string);
+ 
     }
 
     @Given("status for project {string} is {string}")
     public boolean StatusForProjectIs(String projectNumber, String status){
-        app.getProject(projectNumber).status() = status;
+
     }
 
     @Given("developer with name {string} is in app")
     public void DeveloperWithNameIsInApp(String string){
-        developer.developer()
-        developer = app.addDeveloper(string);
+
     }
 
     @Given("{string} is logged in")
     public void IsLoggedIn(String string){
-        app.login(string);
+
     }
 
     @Given("{string} has status project leader for project {string}")
     public boolean HasStatusProjectLeaderForProject(String developer, String ProjectNumber){
-        app.getProject(projectNumber).projectLeader = developer;
+   
     }
 
     @Given("the project {string} has assigned {string}")
     public void TheProjectHasAssigned(String projectNumber, String activity){
-        //få fat i noget app.addActivity()
+
     }
 
     @Given("{string} has status {string}")
     public void HasStats(String activity, String status){
-        project.getActivity(activity).status = status;
+    
     }
 
     @Given("{string} has status developer on project {string}")
